ab421e4d48f986b9211e7052bd0f44262fb71410
nghiath12444 <zznghia2001zz@gmail.com>
2022-03-07 17:39:33 +0700

    feature/complete language

--- calantha-fashion/src/pages/Language/section/Language.js
+++ calantha-fashion/src/pages/Language/section/Language.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import {ChooseLanguageView, DefaultButton} from '../../../component/view'
-import {} from '../../../assets/images'
-import { Image, View, Text, AsyncStorage} from 'react-native'
+import {Image, View, Text, AsyncStorage, ActivityIndicator, Dimensions} from 'react-native'
 
 import {disableTextColor} from '../../../assets/styles'
 import {vietNamFlag, englandFlag, correctIcon} from '../../../assets/images'
 import {useTranslation} from 'react-i18next'
 import i18next from 'i18next'
+
+const {width, height} = Dimensions.get('screen')
 export default function Language(props) {
   const {navigation} = props
   const {t} = useTranslation()
@@ -17,29 +18,47 @@ export default function Language(props) {
     await AsyncStorage.setItem('language', language)
     i18next.changeLanguage(language)
   }
+  useEffect(async () => {
+    const l = await AsyncStorage.getItem('language')
+    setLanguage(l)
+    setLoading(false)
+  }, [])
 
-  return (
-    <View style = {{backgroundColor: disableTextColor, height: '100%'}}>
-      {/*  */}
-      <ChooseLanguageView
-        flag={vietNamFlag}
-        languageName={'vi'}
-        name={t('languages.vi')}
-        language={language}
-        uri={correctIcon}
-      />
-      <ChooseLanguageView
-        flag={englandFlag}
-        languageName={'en'}
-        name={t('languages.vi')}
-        language={language}
-        uri={correctIcon}
-      />
-      <DefaultButton
-        action={SubmitPress()}
-        buttonName = {'Language.submit'}
-      />
+  if (loading) {
+    return (
+      <View
+        style={{
+          width: width,
+          height: height,
+          top: 0,
 
-    </View>
-  )
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ActivityIndicator color="#000000" />
+      </View>
+    )
+  } else {
+    return (
+      <View style={{backgroundColor: disableTextColor, height: '100%'}}>
+        {/*  */}
+        <ChooseLanguageView
+          flag={vietNamFlag}
+          languageName={'vi'}
+          name={t('languages.vi')}
+          language={language}
+          uri={correctIcon}
+        />
+        <ChooseLanguageView
+          flag={englandFlag}
+          languageName={'en'}
+          name={t('languages.vi')}
+          language={language}
+          uri={correctIcon}
+        />
+        <DefaultButton action={SubmitPress()} buttonName={'Language.submit'} />
+      </View>
+    )
+  }
 }
