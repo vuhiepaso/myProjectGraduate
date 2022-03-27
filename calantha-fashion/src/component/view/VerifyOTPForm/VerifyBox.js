a08cb2dc254eb99743683039617cef558bb5215a
philknightz <philknightz@github.com>
2022-03-27 12:48:33 +0700

    remove test

--- calantha-fashion/src/component/view/VerifyOTPForm/VerifyBox.js
+++ calantha-fashion/src/component/view/VerifyOTPForm/VerifyBox.js
@@ -1,8 +1,8 @@
-import React, { forwardRef } from 'react'
-import { View, TextInput } from 'react-native'
+import React, {forwardRef} from 'react'
+import {View, TextInput} from 'react-native'
 import styles from './styles'
 
-function VerifyBox({ setCode, ...other }, ref) {
+function VerifyBox({setCode, ...other}, ref) {
   return (
     <View style={styles.boxContainer}>
       <TextInput
